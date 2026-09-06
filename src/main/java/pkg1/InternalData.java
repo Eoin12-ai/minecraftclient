ff27579b6902be6e707815cc15972cad75478256
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 19:40:27 +0100

    feat: add Swyzzy module InternalData.java

---
+++ src/main/java/pkg1/InternalData.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+final class InternalData {
+   float floatVal;
+   float floatVal2;
+   float floatVal3;
+   boolean bool;
+   final Map<String, Float> map = new HashMap<>();
+   final Map<String, Float> map2 = new HashMap<>();
+}
