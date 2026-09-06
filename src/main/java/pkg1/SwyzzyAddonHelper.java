7ddd59d75f7b5ef7a7f44f76eb840ab88a7cdb54
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 19:41:15 +0100

    feat: add Swyzzy module SwyzzyAddonHelper.java

---
+++ src/main/java/pkg1/SwyzzyAddonHelper.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+public final class SwyzzyAddonHelper {
+   private String name;
+
+   public SwyzzyAddonHelper(String var1) {
+      this.name = var1;
+   }
+
+   public String name() {
+      return this.name;
+   }
+}
