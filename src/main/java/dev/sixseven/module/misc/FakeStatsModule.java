d31c1ec54c10e6ee94a0b04952165deac0657748
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 15:33:44 +0100

    fix: decompiler artifacts in FakeStatsModule.java

--- src/main/java/dev/sixseven/module/misc/FakeStatsModule.java
+++ src/main/java/dev/sixseven/module/misc/FakeStatsModule.java
@@ -161,7 +161,7 @@ public class FakeStatsModule extends Module {
             return false;
          } else {
             String text6 = str5[0];
-            if (text6.startsWith("\\")) {
+            if (text6.startsWith("\")) {
                text6 = text6.substring(1);
             }
 
