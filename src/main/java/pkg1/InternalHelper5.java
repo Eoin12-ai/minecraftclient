f83ac572a116fd148d0643c6af7f1334a5383c7e
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 19:40:55 +0100

    feat: add Swyzzy module InternalHelper5.java

---
+++ src/main/java/pkg1/InternalHelper5.java
@@ -0,0 +1,12 @@
+package pkg1;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface InternalHelper5 {
+   int getInt() default 0;
+}
