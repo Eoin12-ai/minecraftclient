7376dddfe5f099e419ad83ab8dcf8746728445ee
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 19:41:02 +0100

    feat: add Swyzzy module BlockEspPlusModuleData.java

---
+++ src/main/java/pkg1/BlockEspPlusModuleData.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+import net.minecraft.util.math.Vec3d;
+
+public final class BlockEspPlusModuleData {
+   public static Vec3d class243 = Vec3d.ZERO;
+
+   private BlockEspPlusModuleData() {
+   }
+}
