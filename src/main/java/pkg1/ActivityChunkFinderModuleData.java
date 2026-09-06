774622e9aa53b6a43695f1e3101b2b273a92f8ac
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 19:40:47 +0100

    feat: add Swyzzy module ActivityChunkFinderModuleData.java

---
+++ src/main/java/pkg1/ActivityChunkFinderModuleData.java
@@ -0,0 +1,9 @@
+package pkg1;
+
+public final class ActivityChunkFinderModuleData {
+   public ActivityChunkFinderModuleHelper val;
+
+   public ActivityChunkFinderModuleData(ActivityChunkFinderModuleHelper var1) {
+      this.val = var1;
+   }
+}
