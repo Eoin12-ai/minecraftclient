9cd90054ec45bd655cc5d94ab4280f34f81714e4
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 19:41:23 +0100

    feat: add Swyzzy module PacketChunkFinderModuleData.java

---
+++ src/main/java/pkg1/PacketChunkFinderModuleData.java
@@ -0,0 +1,9 @@
+package pkg1;
+
+public final class PacketChunkFinderModuleData extends PacketChunkFinderModuleHelper {
+   public Object object;
+
+   public PacketChunkFinderModuleData(Object var1) {
+      this.object = var1;
+   }
+}
