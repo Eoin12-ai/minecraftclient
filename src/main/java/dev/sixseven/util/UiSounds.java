8c902ccc7901f922e8c05ba9cbe4945a0d29bff8
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 21:28:10 +0100

    fix: UiSounds PositionedSoundInstance.ui 2-arg

--- src/main/java/dev/sixseven/util/UiSounds.java
+++ src/main/java/dev/sixseven/util/UiSounds.java
@@ -22,7 +22,7 @@ public final class UiSounds {
    private static void play(SoundEvent sound, float f, float f4) {
       float f5 = settings.volume();
       if (!(f5 <= 0.01F)) {
-         MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.ui(sound, f, f4 * f5));
+         MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.ui(sound, f));
       }
    }
 
