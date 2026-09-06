a85034e8f29df969644e3b0cdfc338ac8b3aee19
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 15:32:16 +0100

    fix: replace StringConcatFactory indy in ModuleEntry.java

--- src/main/java/dev/sixseven/gui/panel/ModuleEntry.java
+++ src/main/java/dev/sixseven/gui/panel/ModuleEntry.java
@@ -73,8 +73,7 @@ public class ModuleEntry {
         this.module = module;
         this.themes = themes;
         this.state  = state;
-        this.key    = StringConcatFactory.makeConcatWithConstants<"makeConcatWithConstants","rlI">(
-                module.getName(), module.getCategory().name());
+        this.key    = String.valueOf(module.getName(), module.getCategory().name());
         this.expand = new Animation(190.0f, state.isExpanded(key) ? 1.0f : 0.0f);
         this.enable.snapTo(module.isEnabled() ? 1.0f : 0.0f);
 
