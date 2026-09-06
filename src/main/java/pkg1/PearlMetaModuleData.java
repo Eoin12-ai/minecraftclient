59f7b9431a434492ea13c3f31df8b4330b2e1298
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 19:40:57 +0100

    feat: add Swyzzy module PearlMetaModuleData.java

---
+++ src/main/java/pkg1/PearlMetaModuleData.java
@@ -0,0 +1,9 @@
+package pkg1;
+
+public final class PearlMetaModuleData extends PacketChunkFinderModuleHelper {
+   public Object object;
+
+   public PearlMetaModuleData(Object var1) {
+      this.object = var1;
+   }
+}
