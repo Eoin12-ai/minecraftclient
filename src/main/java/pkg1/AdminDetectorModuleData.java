000a2d2d5fa44415420dc5cc7d25ad8efb459b88
Eoin12-ai <Edeltmcallister@icloud.com>
2026-09-06 19:40:36 +0100

    feat: add Swyzzy module AdminDetectorModuleData.java

---
+++ src/main/java/pkg1/AdminDetectorModuleData.java
@@ -0,0 +1,11 @@
+package pkg1;
+
+import net.minecraft.client.gui.DrawContext;
+
+public final class AdminDetectorModuleData {
+   public DrawContext class332;
+
+   public AdminDetectorModuleData(DrawContext var1) {
+      this.class332 = var1;
+   }
+}
